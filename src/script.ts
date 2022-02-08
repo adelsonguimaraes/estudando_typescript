b2a15012d265bcee7e5a88cc588bfeaf914c0d23
adelson.monteiro <adelson.monteiro@fpf.br>
2022-02-08 14:09:25 -0400

    adicionando type e interface, como tipar objetos

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,5 @@
-function firstLetterUpperCase (name: string):string {
-    let firstLetter = name.charAt(0).toUpperCase()
-    return firstLetter+name.substring(1)
+type obj = {
+    nome: string,
+    idade: number,
+    telefone: string|number
 }
-
-firstLetterUpperCase('')
