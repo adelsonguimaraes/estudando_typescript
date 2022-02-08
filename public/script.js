c3de31e672dac1f03f1094f25f3010bd9de50cb4
adelson.monteiro <adelson.monteiro@fpf.br>
2022-02-08 09:43:17 -0400

    start

---
+++ public/script.js
@@ -0,0 +1,10 @@
+var numero1 = document.getElementById('numero1');
+var numero2 = document.getElementById('numero2');
+var botao = document.getElementById('calcular');
+var res = document.getElementById('resultado');
+function calcular(n1, n2) {
+    return n1 + n2;
+}
+botao.addEventListener('click', function () {
+    res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
+});
